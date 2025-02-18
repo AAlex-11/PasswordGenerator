53d076265086d6dc6f08efb56b65a47cbc41e2d8
Razen <Razen13371@outlook.com>
2025-02-18 22:45:39 +0200

    start

---
+++ Module1.vb
@@ -0,0 +1,63 @@
+﻿'INSERT INTO aspnet_Membership (UserId, Password, PasswordSalt, PasswordAnswer) VALUES(@UserId, @Password, @PasswordSalt, @PasswordAnswer);
+'UPDATE aspnet_Membership set Password='', PasswordSalt='', PasswordAnswer='' where UserId='65DAA6BC-381A-4E6C-A2B7-54500E1CCEC9'
+
+'    Generate a random salt (16 bytes).
+'    Combine the salt And password (Or answer) into a Single Byte array.
+'    Compute the HMAC-SHA1 hash Of the combined Byte array.
+'    Combine the salt And the HMAC-SHA1 hash into a Single Byte array.
+'    Encode the final Byte array In Base64.
+
+Imports System
+Imports System.Security.Cryptography
+Imports System.Text
+Module Module1
+    Sub Main()
+        Dim password As String = "YourMomIsALovelyLady"
+        Dim passwordAnswer As String = "YourMomIsALovelyLady"
+
+        ' Generate a salt
+        Dim salt As String = GenerateSalt()
+
+        ' Hash the password with the salt
+        Dim hashedPassword As String = HashPasswordOrAnswer(password, salt)
+
+        ' Hash the password answer
+        Dim hashedPasswordAnswer As String = HashPasswordOrAnswer(passwordAnswer, salt)
+
+        Console.WriteLine("Password: " & hashedPassword)
+        Console.WriteLine("PasswordSalt: " & salt)
+        Console.WriteLine("PasswordAnswer: " & hashedPasswordAnswer)
+        Console.ReadLine()
+    End Sub
+
+    ' Generate a random 16-byte salt
+    Public Function GenerateSalt() As String
+        Dim saltBytes(15) As Byte ' 16 bytes for the salt
+        Using rng As New RNGCryptoServiceProvider()
+            rng.GetBytes(saltBytes)
+        End Using
+        Return Convert.ToBase64String(saltBytes)
+    End Function
+
+    ' Hash the password or answer using the ASP.NET Membership algorithm
+    Public Function HashPasswordOrAnswer(input As String, salt As String) As String
+        ' Decode the salt from Base64
+        Dim saltBytes As Byte() = Convert.FromBase64String(salt)
+
+        ' Convert the input (password or answer) to bytes (Unicode encoding)
+        Dim inputBytes As Byte() = Encoding.Unicode.GetBytes(input)
+
+        ' Combine the salt and input bytes
+        Dim combinedBytes(saltBytes.Length + inputBytes.Length - 1) As Byte
+        Buffer.BlockCopy(saltBytes, 0, combinedBytes, 0, saltBytes.Length)
+        Buffer.BlockCopy(inputBytes, 0, combinedBytes, saltBytes.Length, inputBytes.Length)
+
+        ' Compute the SHA-1 hash of the combined bytes
+        Using sha1 As SHA1 = SHA1.Create()
+            Dim hashBytes As Byte() = sha1.ComputeHash(combinedBytes)
+
+            ' Encode only the hash in Base64 (not the salt)
+            Return Convert.ToBase64String(hashBytes)
+        End Using
+    End Function
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PasswordGenerator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PasswordGenerator")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("23aa1bfc-94c4-465b-b8a5-a85405da1318")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
